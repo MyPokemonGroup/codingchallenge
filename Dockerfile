e9048abf678d831c29c53de17009acb21afa942f
Charles Weng <cweng8849@gmail.com>
2018-08-24 12:30:24 +0000

    Fixing up check_price script to work inside docker

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,26 @@
-FROM node:latest
+FROM node:alpine
+
+WORKDIR /usr/src/app
+
+RUN apk update
+#RUN apk add gcc musl-dev libxslt-dev
+RUN apk add --no-cache python python-dev python3 python3-dev \
+    libexif udev xvfb chromium chromium-chromedriver \
+    linux-headers build-base bash git ca-certificates && \
+    python3 -m ensurepip && \
+    rm -r /usr/lib/python*/ensurepip && \
+    pip3 install --upgrade pip setuptools && \
+    if [ ! -e /usr/bin/pip ]; then ln -s pip3 /usr/bin/pip ; fi && \
+    pip3 install selenium && \
+    pip3 install pyvirtualdisplay && \
+    pip3 install xvfbwrapper && \
+    rm -r /root/.cache
+RUN apk add py3-lxml
+
 
 LABEL maintainer="MyPokemonGroup"
 
-WORKDIR /usr/src/app
+COPY ./lib/chromedriver /usr/src/app/lib/chromedriver
 
 VOLUME [ "/usr/src/app" ]
 
--- lib/check_price.py
+++ lib/check_price.py
@@ -1,11 +1,21 @@
 import os
 import sys
+from pyvirtualdisplay import Display
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from lxml import html
 
+
 def get_price(url):
-  browser = webdriver.Chrome(executable_path=os.path.join(os.getcwd(), "chromedriver"))
+  #browser = webdriver.Chrome(executable_path=os.path.join(os.getcwd(), "chromedriver"))
+  chrome_options = Options()
+  chrome_options.add_argument('--headless')
+  chrome_options.add_argument('--no-sandbox')
+  chrome_options.add_argument('--disable-dev-shm-usage')
+  display = Display(visible=0, size=(800,800))
+  display.start()
+  browser = webdriver.Chrome(chrome_options=chrome_options)
   browser.get(url)
   delay = 10000
   WebDriverWait(browser, delay)
@@ -14,6 +24,8 @@ def get_price(url):
   cents = tree.xpath("//sup[@class='sx-price-fractional']/text()")
   total_price = '$' + price[0] + '.' + cents[0]
   print(total_price)
+  #browser.quit()
+  display.stop()
   return total_price
   
 
